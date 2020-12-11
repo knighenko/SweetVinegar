fc5eb651d831f5d2fdf701fae1270775c23c571a
Maksym Knyzhenko <knighenko@gmail.com>
2020-12-11 21:42:33 +0200

    Add connectToserverMethod

--- app/src/main/java/com/knighenko/sweetvinegar/activities/MainActivity.java
+++ app/src/main/java/com/knighenko/sweetvinegar/activities/MainActivity.java
@@ -3,14 +3,47 @@ package com.knighenko.sweetvinegar.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.knighenko.sweetvinegar.R;
+import com.knighenko.sweetvinegar.model.ConnectServer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    // private static final String SERVER_IP = "91.235.129.33";
+    private static final String SERVER_IP = "10.0.2.2";
+    private static final int PORT = 8080;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectToServerSearch();
     }
+
+    /**
+     * Метод соединяется  сервером
+     */
+
+    private void connectToServerSearch() {
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    ConnectServer connectServer = new ConnectServer(SERVER_IP, PORT);
+                    String string = connectServer.readJsonString("https://www.olx.ua/elektronika/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15");
+                    System.out.println(string);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+    }
+
 }
--- app/src/main/java/com/knighenko/sweetvinegar/model/ConnectServer.java
+++ app/src/main/java/com/knighenko/sweetvinegar/model/ConnectServer.java
@@ -1,4 +1,38 @@
 package com.knighenko.sweetvinegar.model;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ConnectServer {
+    private final Socket socket;
+    private DataOutputStream outputStream;
+    private DataInputStream inputStream;
+
+    public ConnectServer(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+    }
+    /**Метод для соединения с сервером, возвращает строку в виде Json
+     * @param request - адрес URL*/
+
+    public String readJsonString(String request) throws IOException {
+        outputStream = new DataOutputStream(socket.getOutputStream());
+        System.out.println("Client connected to server");
+        while (!socket.isOutputShutdown()) {
+            outputStream.writeUTF(request);
+            outputStream.flush();
+
+            inputStream = new DataInputStream(socket.getInputStream());
+
+            String message = inputStream.readUTF();
+            System.out.println("Response from server is: " + message);
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            System.out.println("Client disconected!");
+            return message;
+        }
+        return null;
+    }
 }
