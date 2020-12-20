1d274c415f9cb003a8a898a7a7060586d200d925
Maksym Knyzhenko <knighenko@gmail.com>
2020-12-20 21:20:23 +0200

    Add AdvertisementActivity.java

---
+++ app/src/main/java/com/knighenko/sweetvinegar/activities/AdvertisementActivity.java
@@ -0,0 +1,14 @@
+package com.knighenko.sweetvinegar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvertisementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement);
+    }
+}
