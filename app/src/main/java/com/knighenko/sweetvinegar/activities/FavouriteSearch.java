311f306e2deccd6e9c0225a82cf62a14aff15f5f
Maksym Knyzhenko <knighenko@gmail.com>
2020-12-22 23:24:59 +0200

    Add FavouriteSearch.java

---
+++ app/src/main/java/com/knighenko/sweetvinegar/activities/FavouriteSearch.java
@@ -0,0 +1,16 @@
+package com.knighenko.sweetvinegar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.knighenko.sweetvinegar.R;
+
+public class FavouriteSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_search);
+    }
+}
