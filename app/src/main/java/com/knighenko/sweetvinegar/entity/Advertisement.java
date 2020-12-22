daaec1280ac04472b61154d8deae10f4f73d303f
Maksym Knyzhenko <knighenko@gmail.com>
2020-12-22 23:22:59 +0200

    Add field in Advertisement.java

--- app/src/main/java/com/knighenko/sweetvinegar/entity/Advertisement.java
+++ app/src/main/java/com/knighenko/sweetvinegar/entity/Advertisement.java
@@ -1,4 +1,8 @@
 package com.knighenko.sweetvinegar.entity;
 
 public class Advertisement {
+    private String title;
+    private String url;
+    private String imageSrc;
+    private String description;
 }
