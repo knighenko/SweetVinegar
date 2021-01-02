f65b9aa1a578b29600feaf1f9dab4aeef3f7bd9d
Maksym Knyzhenko <knighenko@gmail.com>
2021-01-02 23:14:16 +0200

    Add method btnEnter onclick

--- app/src/main/java/com/knighenko/sweetvinegar/activities/MainActivity.java
+++ app/src/main/java/com/knighenko/sweetvinegar/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.knighenko.sweetvinegar.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import com.knighenko.sweetvinegar.R;
@@ -19,31 +21,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connectToServerSearch();
+        connectToServerSearch("https://www.olx.ua/elektronika/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15");
     }
 
     /**
-     * Метод соединяется  сервером
+     * Method connecting to server with parameter Request
      */
 
-    private void connectToServerSearch() {
+    private void connectToServerSearch(String request) {
 
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
+        new Thread(() -> {
+            try {
 
-                    ConnectServer connectServer = new ConnectServer(SERVER_IP, PORT);
-                    String string = connectServer.readJsonString("https://www.olx.ua/elektronika/kharkov/?search%5Bfilter_float_price%3Afrom%5D=free&search%5Bdist%5D=15");
-                    System.out.println(string);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+                ConnectServer connectServer = new ConnectServer(SERVER_IP, PORT);
+                String string = connectServer.readJsonString(request);
+                System.out.println(string);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+
         }).start();
     }
 
+    /**
+     * Method doing when button Enter (Вход) onClick
+     */
+    public void btnEnter(View view) {
+        String e_mail;
+        String password;
+        e_mail = ((EditText) findViewById(R.id.edit_text_e_mail)).getText().toString();
+        password = ((EditText) findViewById(R.id.edit_text_password)).getText().toString();
+        connectToServerSearch(e_mail + "=" + password);
+    }
 }
