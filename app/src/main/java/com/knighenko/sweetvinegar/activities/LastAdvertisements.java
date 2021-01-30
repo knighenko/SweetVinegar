378d4846586a561c4e884ebb17d0ebb5a36f7286
Maksym Knyzhenko <knighenko@gmail.com>
2021-01-30 21:42:27 +0200

    Implemented AdvAdapter.java

--- app/src/main/java/com/knighenko/sweetvinegar/activities/LastAdvertisements.java
+++ app/src/main/java/com/knighenko/sweetvinegar/activities/LastAdvertisements.java
@@ -1,29 +1,37 @@
 package com.knighenko.sweetvinegar.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.knighenko.sweetvinegar.R;
+import com.knighenko.sweetvinegar.adapter.AdvAdapter;
 import com.knighenko.sweetvinegar.entity.Advertisement;
 import com.knighenko.sweetvinegar.model.JsonToObject;
 
 import java.util.ArrayList;
 
 public class LastAdvertisements extends AppCompatActivity {
+    private RecyclerView listAdvRecyclerView;
+    private AdvAdapter advAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_advertisements);
         String jsonString = getIntent().getStringExtra("jsonString");
-
-
-
-        ArrayList<Advertisement> advertisements= JsonToObject.getAdvertisements(jsonString);
-               for (Advertisement advertisement:advertisements            ) {
-                 System.out.println(advertisement.getId());
+        ArrayList<Advertisement> advertisementsList = JsonToObject.getAdvertisements(jsonString);
+        for (Advertisement advertisement : advertisementsList) {
+            System.out.println(advertisement.getId());
         }
+        listAdvRecyclerView = findViewById(R.id.recyclerTenAdv);
+        listAdvRecyclerView.setHasFixedSize(true);
+        listAdvRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        advAdapter = new AdvAdapter();
+        advAdapter.setListAdv(advertisementsList);
+        listAdvRecyclerView.setAdapter(advAdapter);
 
 
     }
--- app/src/main/java/com/knighenko/sweetvinegar/adapter/AdvAdapter.java
+++ app/src/main/java/com/knighenko/sweetvinegar/adapter/AdvAdapter.java
@@ -1,4 +1,77 @@
 package com.knighenko.sweetvinegar.adapter;
 
-public class AdvAdapter {
+import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.knighenko.sweetvinegar.R;
+import com.knighenko.sweetvinegar.entity.Advertisement;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class AdvAdapter extends RecyclerView.Adapter<AdvAdapter.ViewHolder> {
+
+    private ArrayList<Advertisement> advertisementsList = new ArrayList<>();
+
+
+    @NonNull
+    @Override
+    public AdvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_advertisement, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdvAdapter.ViewHolder holder, int position) {
+        holder.bind(advertisementsList.get(position));
+    }
+
+    public void setListAdv(Collection<Advertisement> advertisements) {
+        this.advertisementsList.addAll(advertisements);
+        notifyDataSetChanged();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return advertisementsList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView titleView;
+        private TextView descriptionView;
+        private ImageView imageView;
+
+        ViewHolder(View view) {
+            super(view);
+            titleView = (TextView) view.findViewById(R.id.textViewTitleAdv);
+            descriptionView = (TextView) view.findViewById(R.id.textViewDescription);
+            imageView = (ImageView) view.findViewById(R.id.bigImageViewAdv);
+        }
+
+        public void bind(Advertisement adv) {
+            titleView.setText(adv.getTitle());
+            titleView.setPaintFlags(titleView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            descriptionView.setText(adv.getDescription());
+            String urlImg = adv.getImageSrc();
+            paintImg(urlImg);
+        }
+
+        private void paintImg(String urlImg) {
+            if (urlImg.equals("No Img")) {
+                imageView.setImageResource(R.drawable.no_image);
+            } else {
+                Picasso.get().load(urlImg).resize(200, 200).centerCrop().into(imageView);
+            }
+        }
+    }
 }
